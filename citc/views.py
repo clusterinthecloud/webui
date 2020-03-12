03a4d4afcfa2e9dd7addb8629e57fa6ba0a2d7eb
Matt Williams <matt@milliams.com>
2020-03-12 11:04:41 +0000

    Display LDAP users

--- citc/views.py
+++ citc/views.py
@@ -1,5 +1,10 @@
+from copy import copy
+
+import yaml
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from ldap3 import Server, Connection, MOCK_SYNC, ALL_ATTRIBUTES
 
 
 @login_required
@@ -9,4 +14,36 @@ def index(request):
 
 @login_required
 def users(request):
-    return render(request, "users.html")
+    base_dn = "ou=People,dc=citc,dc=acrc,dc=bristol,dc=ac,dc=uk"
+    if settings.IN_PRODUCTION:
+        with open("/etc/citc/webui.yaml", "r") as f:
+            config = yaml.safe_load(f)
+        connection = Connection('ldap://localhost', user='cn=Directory Manager', password=config["ldap_password"], auto_bind=True)
+    else:
+        server = Server('my_fake_server')
+        connection = Connection(server, user='cn=Directory Manager', password='my_password', client_strategy=MOCK_SYNC)
+        connection.strategy.add_entry(f'cn=matt,{base_dn}',
+                                      {
+                                          'objectClass': [
+                                              'top',
+                                              'person',
+                                              'organizationalPerson',
+                                              'inetOrgPerson',
+                                              'posixAccount',
+                                          ],
+                                          'cn': 'matt',
+                                          'givenName': 'Matt',
+                                          'sn': 'Williams',
+                                          'uid': 'matt',
+                                          'uidNumber': 10001,
+                                          'gidNUmber': 100,
+                                          'homeDirectory': '/mnt/shared/home/matt',
+                                          'loginShell': '/bin/bash',
+                                      })
+        connection.bind()
+    connection.search('ou=People,dc=citc,dc=acrc,dc=bristol,dc=ac,dc=uk', '(objectclass=posixAccount)', attributes=ALL_ATTRIBUTES)
+    users = copy(connection.entries)
+
+    context = {"users": users}
+
+    return render(request, "users.html", context)
